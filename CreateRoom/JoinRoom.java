cfc9aebb6c052c7085cd7799a4256a83c2fd46a5
SM922 <ddi03090@naver.com>
2021-05-12 14:04:58 +0900

    test

---
+++ CreateRoom/JoinRoom.java
@@ -0,0 +1,26 @@
+package CreateRoom;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class JoinRoom {
+
+	public JoinRoom(JFrame Jframe){
+		
+		Jframe = new JFrame();
+		Jframe.getContentPane().setBackground(Color.WHITE);
+		Jframe.setBackground(Color.WHITE);
+		Jframe.setBounds(100, 100, 1600, 1200);
+		Jframe.setLocationRelativeTo(Jframe);
+		Jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Jframe.getContentPane().setLayout(null);
+		Jframe.setVisible(true);
+		
+		
+		
+	}
+	
+	
+	
+}
