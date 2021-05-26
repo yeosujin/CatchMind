29b905e4f528fe0a0dd8ad751f2af86c8f5fd9dd
yeosujin <58133978+yeosujin@users.noreply.github.com>
2021-05-26 14:03:37 +0900

    Add files via upload

---
+++ CatchMind_Client/Main.java
@@ -0,0 +1,13 @@
+package CatchMind_Client;
+
+public class Main {
+	public static final int SCREEN_WIDTH = 1280;
+	public static final int SCREEN_HEIGHT = 720;
+	public static final int Port = 10000;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new CatchMind();
+	}
+
+}
